5b51b16ff8c6643612af593235283320641e3e98
unknown <518arifulislam518@gmail.com>
2026-09-23 14:43:04 +0600

    added fix error

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -4,7 +4,7 @@ import DestinationCard from "./DestinationCard";
 const Featured = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/featured`);
   const destinations = await res.json();
-  console.log(destinations);
+  // console.log(destinations);
   return (
     <div className="w-9/12 mx-auto">
       <h1 className="mt-10 text-4xl font-bold ">Featured Destinations</h1>
